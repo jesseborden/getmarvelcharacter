907b812f8a33637b0613c68ad132c0746e24c698
Jesse Borden <bordenjj@bonaventure.edu>
2018-07-07 12:13:01 -0400

    Add some statements to ensure only valid API responses from weather and marvel API are processed.

--- GetMarvelCharacter/MainPage.xaml.cs
+++ GetMarvelCharacter/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -11,6 +12,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,6 +27,52 @@ namespace GetMarvelCharacter
         public MainPage()
         {
             this.InitializeComponent();
+            try
+            {
+                GetMarvelCharacter();
+            }
+            catch
+            {
+                LblError.Text = "can't set marvel character. Is the network connection down?";
+            }
         }
+        private void GetMarvelCharacter()
+        {
+
+            var backgroundtimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(30)
+            };
+            backgroundtimer.Tick += async (o, e) =>
+            {
+                MarvelAPI.RootObject mycharacter = await MarvelAPI.GetMarvelCharacter.GetCharacter(1009610);
+
+                //Change Background to Iron Man
+                if (mycharacter != null)
+                {
+                    int resultindex = (mycharacter.data.results.Count) - 1;
+                    Random random = new Random();
+                    int randomindex = random.Next(0, resultindex);
+                    string badimage = "image_not_available";
+                    string imageurl = mycharacter.data.results[randomindex].thumbnail.path + "." + mycharacter.data.results[randomindex].thumbnail.extension;
+                    if (imageurl.Contains(badimage))
+                    {
+                        imgBackground.Source = new BitmapImage(new Uri(this.BaseUri, "Assets/Backgrounds/wonderwoman.png"));
+                        LblCharacter.Text = "Wonder Woman :)";
+                    }
+                    if (!imageurl.Contains(badimage))
+                    {
+                        imgBackground.Source = new BitmapImage(new Uri(imageurl, UriKind.Absolute));
+                        LblCharacter.Text = mycharacter.data.results[randomindex].name;
+                    }
+                }
+                else
+                {
+                    imgBackground.Source = new BitmapImage(new Uri(this.BaseUri, "Assets/Backgrounds/wonderwoman.png"));
+                    LblCharacter.Text = "Wonder Woman :)";
+                }
+                };
+                backgroundtimer.Start();
+    }
     }
 }
---
+++ GetMarvelCharacter/MarvelAPI/GetMarvelCharacter.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace GetMarvelCharacter.MarvelAPI
+{
+    class GetMarvelCharacter
+    {
+        private const string publickey = "your public key";
+        private const string privatekey = "your private key";
+
+        public async static Task<RootObject> GetCharacter(int CharacterID)
+        {
+            try
+            {
+                Random random = new Random();
+                int offset = random.Next(0, 1400);
+                string requesturl = "https://gateway.marvel.com:443/v1/public/characters?offset=" + offset + CreateHash();
+                System.Diagnostics.Debug.WriteLine(requesturl);
+                var http = new HttpClient();
+                var response = await http.GetAsync(requesturl);
+                var result = await response.Content.ReadAsStringAsync();
+                var serializer = new DataContractJsonSerializer(typeof(RootObject));
+                var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+                if (response.IsSuccessStatusCode == true)
+                {
+                    var data = (RootObject)serializer.ReadObject(ms);
+                    return data;
+                }
+                else
+                {
+                    return null;
+                }
+               
+            }
+            catch (SerializationException)
+            {
+                throw new SerializationException("You can't authenticate to the API");
+            }
+        }
+
+        private static string CreateHash()
+        {
+            var timestamp = DateTime.Now.Ticks.ToString();
+            var toBeHashed = timestamp + privatekey + publickey;
+            var hashedmessage = ComputeMD5(toBeHashed);
+            var urlsuffix = "&ts=" + timestamp + "&apikey=" + publickey + "&hash=" + hashedmessage;
+            return urlsuffix;
+        }
+        private static string ComputeMD5(string str)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
+            IBuffer buff = CryptographicBuffer.ConvertStringToBinary(str, BinaryStringEncoding.Utf8);
+            var hashed = alg.HashData(buff);
+            var res = CryptographicBuffer.EncodeToHexString(hashed);
+            return res;
+
+        }
+    }
+    [DataContract]
+    public class Thumbnail
+    {
+        [DataMember]
+        public string path { get; set; }
+        [DataMember]
+        public string extension { get; set; }
+    }
+    [DataContract]
+    public class Comics
+    {
+        [DataMember]
+        public int available { get; set; }
+        [DataMember]
+        public string collectionURI { get; set; }
+        [DataMember]
+        public List<object> items { get; set; }
+        [DataMember]
+        public int returned { get; set; }
+    }
+    [DataContract]
+    public class Series
+    {
+        [DataMember]
+        public int available { get; set; }
+        [DataMember]
+        public string collectionURI { get; set; }
+        [DataMember]
+        public List<object> items { get; set; }
+        [DataMember]
+        public int returned { get; set; }
+    }
+    [DataContract]
+    public class Stories
+    {
+        [DataMember]
+        public int available { get; set; }
+        [DataMember]
+        public string collectionURI { get; set; }
+        [DataMember]
+        public List<object> items { get; set; }
+        [DataMember]
+        public int returned { get; set; }
+    }
+    [DataContract]
+    public class Events
+    {
+        [DataMember]
+        public int available { get; set; }
+        [DataMember]
+        public string collectionURI { get; set; }
+        [DataMember]
+        public List<object> items { get; set; }
+        [DataMember]
+        public int returned { get; set; }
+    }
+    [DataContract]
+    public class Url
+    {
+        [DataMember]
+        public string type { get; set; }
+        [DataMember]
+        public string url { get; set; }
+    }
+    [DataContract]
+    public class Result
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string description { get; set; }
+        //  [DataMember]
+        //  public DateTime modified { get; set; }
+        [DataMember]
+        public Thumbnail thumbnail { get; set; }
+        [DataMember]
+        public string resourceURI { get; set; }
+        [DataMember]
+        public Comics comics { get; set; }
+        [DataMember]
+        public Series series { get; set; }
+        [DataMember]
+        public Stories stories { get; set; }
+        [DataMember]
+        public Events events { get; set; }
+        [DataMember]
+        public List<Url> urls { get; set; }
+    }
+    [DataContract]
+    public class Data
+    {
+        [DataMember]
+        public int offset { get; set; }
+        [DataMember]
+        public int limit { get; set; }
+        [DataMember]
+        public int total { get; set; }
+        [DataMember]
+        public int count { get; set; }
+        [DataMember]
+        public List<Result> results { get; set; }
+    }
+    [DataContract]
+    public class RootObject
+    {
+        [DataMember]
+        public int code { get; set; }
+        [DataMember]
+        public string status { get; set; }
+        [DataMember]
+        public string copyright { get; set; }
+        [DataMember]
+        public string attributionText { get; set; }
+        [DataMember]
+        public string attributionHTML { get; set; }
+        [DataMember]
+        public string etag { get; set; }
+        [DataMember]
+        public Data data { get; set; }
+    }
+}
+    
